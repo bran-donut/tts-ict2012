a67d9276e6c3558002b6044c516ce9bd9f73da45
Brandon <brandonlimjingzhi@gmail.com>
2022-10-31 23:48:50 +0800

    updated tailwind configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+    },
+    extend: {
+      fontFamily: {
+        roboto: ["'Roboto', sans-serif", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        "tts-black": "#1f2733",
+        "tts-red": "#ac3e3f",
+        "tts-bg": "#f0f2f5",
+      },
+    },
   },
   plugins: [],
 };
