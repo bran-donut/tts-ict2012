ca0c0567637541a453f1230d33e7108d58351b8c
brandonut <heelabc@gmail.com>
2022-11-04 15:58:53 +0800

    added record page

--- pages/inventory.js
+++ pages/inventory.js
@@ -22,18 +22,6 @@ export default function Inventory() {
           <button onClick={() => setIndex(1)} className={`${tab[index] == tab[1] ? "text-blue-600 pb-4 font-bold border-b-2 border-indigo-500" : "text-black"} mx-10 text-md md:text-base`}>{`${tab == 'null' ? "" : tab[1]}`}</button>
         </div>
       </SubHeader>
-      {/* <div className="flex flex-col items-start justify-start w-full px-10 pt-5 bg-white">
-        <Breadcrumb
-          breadCrumbItems={["Home", "Inventory"]}
-        />
-        <h2 className="pb-3 text-3xl font-medium">{`${header ? header[index] : ""}`}</h2>
-        <p className="mt-2 text-sm text-gray-500 md:text-base">Displays all equipment inside the system</p>
-        <div className="py-3">
-          <button onClick={() => setIndex(0)} className={`${tab[index] == tab[0] ? "text-blue-600 font-bold border-b-2 border-indigo-500" : "text-black"} text-md md:text-base`}>{`${tab == 'null' ? "" : tab[0]}`}</button>
-          <button onClick={() => setIndex(1)} className={`${tab[index] == tab[1] ? "text-blue-600 font-bold border-b-2 border-indigo-500" : "text-black"} mx-10 text-md md:text-base`}>{`${tab == 'null' ? "" : tab[1]}`}</button>
-        </div>
-      </div> */}
-
       <section className={`${tab[index] == "Scope" ? "visible" : "invisible"} bg-[#C1C1C1] px-20`}>
         <div className="grid grid-cols-1 overflow-y-scroll bg-[#F0F2F5] xl:grid-cols-2 max-h-96 px-10">
           {equipmentData.map((e, i) => <EquipmentCard equipmentData={equipmentData[i]} key={i} />)}
