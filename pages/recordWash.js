ef272d38f04a6f637d01475138b53205fbdd0ba3
brandonut <heelabc@gmail.com>
2022-11-05 13:13:13 +0800

    added modals for record pages

--- pages/recordWash.js
+++ pages/recordWash.js
@@ -6,9 +6,12 @@ import MainHeader from "../components/MainHeader";
 import SubHeader from "../components/SubHeader";
 import MobileScan from "../components/MobileScan";
 import DateInput from "../components/DateInput";
+import Modal from "../components/Modal";
 
 export default function RecordWash() {
 
+  const [showExitModal, setShowExitModal] = useState(false);
+  const [showContinueModal, setShowContinueModal] = useState(false);
   const [equipmentData, setEquipmentData] = useState(
     {
         brand: "Olympus", scopeType: "tracheal intubation", modelNumber: "TJF403", serialNumber: "21904890", status: "New"
@@ -127,18 +130,39 @@ export default function RecordWash() {
               </div>
 
                   <div className="flex flex-col items-center justify-end w-full gap-0 px-5 py-5 bg-white md:flex-row md:gap-3">
-                    <a href="/trip" className="text-black hover:text-black/80 hover:cursor-pointer hover:underline">
-                      Back
+                    <a className="text-black hover:text-black/80 hover:cursor-pointer hover:underline">
+                      Previous Step
                     </a>
-                    <button type="submit" className="px-10 py-2 ml-4 transition-colors duration-150 bg-white border-2 rounded-sm text-tts-red hover:bg-tts-red/80 border-tts-red">
-                      Add new
+                    <button type="submit" onClick={() => setShowExitModal(true)} className="px-10 py-2 ml-4 transition-colors duration-150 bg-white border-2 rounded-sm text-tts-red hover:bg-tts-red/80 border-tts-red">
+                      Save & Exit
                     </button>
-                    <button type="submit" onClick={() => setStatus("edit")} className="px-10 py-2 text-white transition-colors duration-150 border-2 rounded-sm bg-tts-red hover:bg-tts-red/80 border-tts-red">
-                      Submit
+                    <button type="submit" onClick={() => setShowContinueModal(true)} className="px-10 py-2 text-white transition-colors duration-150 border-2 rounded-sm bg-tts-red hover:bg-tts-red/80 border-tts-red">
+                      Save & Continue
                     </button>
                   </div>
             </form>
           </section>
+
+          {(showExitModal ?
+            <Modal
+              heading="Save & Exit ?"
+              description="Are you sure you want to save and Exit?"
+              leftText="Cancel"
+              rightText="Save & Exit"
+              onClickClose={()=> setShowExitModal(false)}
+            />
+          : null)}
+
+          {(showContinueModal ?
+            <Modal
+              heading="Save & Continue ?"
+              description="Are you sure you want to save and continue?"
+              leftText="Cancel"
+              rightText="Save & Continue"
+              onClickClose={()=> setShowContinueModal(false)}
+            />
+          : null)}
+
     </Layout>
   );
 }
