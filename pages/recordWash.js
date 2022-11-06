58c3fb8dfad072ef22a1f04b7ac342efa3106351
brandonut <heelabc@gmail.com>
2022-11-06 21:39:51 +0800

    update

--- pages/recordWash.js
+++ pages/recordWash.js
@@ -22,11 +22,7 @@ export default function RecordWash() {
 
   return (
     <Layout>
-<<<<<<< HEAD
       <MainHeader heading="Record" description="Equipment Sampling Record" />
-=======
-      <MainHeader heading="Inventory" description="View all the equipment and miscellaneous inside the system" details={[{ title: 'Total Equipment in Inventory', subtitle: equipments.length }]} />
->>>>>>> 4077d9f0bf2343fc09c41bda95d7d10581f3d8ad
       <SubHeader
       heading={equipmentData.brand + " " + equipmentData.modelNumber + " " + equipmentData.serialNumber}
       description="Record the details of equipment sampling"
