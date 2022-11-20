0c6372836716347a2c00d1dc71b783652b849ab4
p1816246 <khorchai@hotmail.co.uk>
2022-11-20 16:20:32 +0800

    equipment form edit

--- components/ActionButton.js
+++ components/ActionButton.js
@@ -1,13 +1,12 @@
 import { useState } from "react";
 
-export default function ActionButton({ index, active, name, icon, onClickAction, disable = false, subHeaderButton = false }) {
+export default function ActionButton({ index, active, name, icon, onClickAction, disable = false }) {
     const [hover, setHover] = useState(false);
   
     return (
       <button
         className={`
                   px-2 flex items-center gap-2 border 
-                  ${subHeaderButton ? "border-[#FF9193] text-tts-red" : ""}
                   ${(active && !disable) || hover ? "border-[#FF9193] text-tts-red bg-[#FF9193]/30" : "border-gray-400 text-black"}
                   ${disable ? "border-gray-400 text-gray-400 cursor-default" : ""}
               `}
