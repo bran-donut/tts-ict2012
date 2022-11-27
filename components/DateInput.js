bbec6ec0cfc4adfebc6e3edc9f83a92557da6d6b
p1816246 <khorchai@hotmail.co.uk>
2022-11-27 19:22:23 +0800

    date restrict

--- components/DateInput.js
+++ components/DateInput.js
@@ -6,6 +6,22 @@ import { useState, useEffect } from "react";
 export default function Input({ menuHeader, tooltipText, saveState, index, inputValue }) {
   const [saveText, setSaveText] = useState('');
 
+  const restrictDate = () => {
+    var today = new Date();
+
+    var month = today.getMonth() + 1;
+    var day = today.getDate();
+    var year = today.getFullYear();
+
+    if(month < 10)
+        month = '0' + month.toString();
+    if(day < 10)
+        day = '0' + day.toString();
+
+    var maxDate = year + '-' + month + '-' + day; 
+    return maxDate;
+  }
+
   useEffect(() => {
     setSaveText(inputValue ? inputValue : '');
   }, [inputValue])
@@ -26,7 +42,7 @@ export default function Input({ menuHeader, tooltipText, saveState, index, input
               <InfoCircleOutlined style={{fontSize: '16px', color: 'rgb(107 114 128)' }}/>
           </Tooltip>
       </div>
-    <input onChange={e => setSaveText(e.target.value)} value={saveText} className="relative flex items-center w-full p-2 border-2 border-gray-300 rounded-md input-group" type="date" placeholder="Select"/>   
+    <input onChange={e => setSaveText(e.target.value)} value={saveText} max={saveState == "cleanDateOfCollection" ? restrictDate() : ''} className="relative flex items-center w-full p-2 border-2 border-gray-300 rounded-md input-group" type="date" placeholder="Select"/>   
   </div>
   );
 }
